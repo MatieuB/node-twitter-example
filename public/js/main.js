e2d507d17bbce8ad95d18d7f6e432268a1fc8b52
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-08 11:34:01 -0600

    twitter one hour hackathon

--- public/js/main.js
+++ public/js/main.js
@@ -3,51 +3,101 @@ var twitterStream = angular.module('myApp', ['chart.js'])
 twitterStream.controller("mainCtrl", ['$scope', 'socket',
 function ($scope, socket) {
   //chart labels
-  $scope.labels = ["iPhone", "iPad", "Android", "Web Client", "Other"];
+  $scope.labels = ["Trump", "Bernie", "Hillary"];
   //chart colors
-  $scope.colors = ['#6c6a6c','#000000','#7FFD1F','#EC872A', '#9527C2'];
+  $scope.colors = ['#6c6a6c','#000000','#7FFD1F'];
   //intial data values
-  $scope.trumpData = [0,0,0,0,0];
-  $scope.sandersData = [0,0,0,0,0];
+  $scope.pplData = {trump: {numTallied:0, totalFollowers:0}, bernie: {numTallied:0, totalFollowers:0}, hillary: {numTallied:0, totalFollowers:0}};
+
+
+  function sortByFollowers(a,b){
+    return b.followers - a.followers;
+  }
+
+  $scope.mostPopular = {
+    trump: [],
+    bernie: [],
+    hillary: []
+  }
+
+  $scope.followersData = [0,0,0]
 
   socket.on('newTweet', function (tweet) {
     $scope.tweet = tweet.text
     $scope.user = tweet.user.screen_name
-    //parse source from payload
-    var source = tweet.source.split('>')[1].split('<')[0].split(' ')[2]
+    //parse followers from payload
+    var followers = tweet.user.followers_count;
     //all hashtags in the tweet
     var hashtags = tweet.entities.hashtags.map(function(el){
       return el.text.toLowerCase()
     })
 
-    //check source and increment for #trump tweets
-    if (hashtags.indexOf('trump') !== -1){
-      switch (source) {
-        case 'iPhone': $scope.trumpData[0]++
-        break;
-        case 'iPad': $scope.trumpData[1]++
-        break;
-        case 'Android': $scope.trumpData[2]++
-        break;
-        case 'Web': $scope.trumpData[3]++
-        break;
-        default: $scope.trumpData[4]++
+    //check followers and increment for #trump tweets
+    if (hashtags.includes('trump')){
+      var trump = $scope.pplData.trump;
+      trump.numTallied++;
+      trump.totalFollowers += +followers;
+      $scope.followersData[0] = trump.totalFollowers / trump.numTallied;
+      var popularTrump = $scope.mostPopular.trump
+      console.log(tweet.text);
+      var newPost = {message: $scope.tweet, followers: +followers, user: tweet.user.screen_name}
+      if(popularTrump.length === 0){
+        popularTrump.push(newPost)
+      }else if (popularTrump.length >= 5 && popularTrump[4].followers < followers){
+        popularTrump.pop();
+        popularTrump.push(newPost);
+        popularTrump.sort(sortByFollowers);
+      }else if (popularTrump.length < 5){
+        popularTrump.push(newPost);
+        popularTrump.sort(sortByFollowers);
       }
+      console.log(popularTrump);
     }
 
-    //check source and increment for #feelthebern tweets
-    else if (hashtags.indexOf('feelthebern') !== -1) {
-      switch (source) {
-        case 'iPhone': $scope.sandersData[0]++
-        break;
-        case 'iPad': $scope.sandersData[1]++
-        break;
-        case 'Android': $scope.sandersData[2]++
-        break;
-        case 'Web': $scope.sandersData[3]++
-        break;
-        default: $scope.sandersData[4]++
+    //check followers and increment for #feelthebern tweets
+    else if (hashtags.includes('feelthebern')) {
+      var bernie = $scope.pplData.bernie;
+      bernie.numTallied++;
+      bernie.totalFollowers += +followers;
+      $scope.followersData[1] = bernie.totalFollowers / bernie.numTallied;
+
+      var popularBernie = $scope.mostPopular.bernie
+      console.log(tweet.text);
+      var newPost = {message: $scope.tweet, followers: +followers, user: tweet.user.screen_name}
+      if(popularBernie.length === 0){
+        popularBernie.push(newPost)
+      }else if (popularBernie.length >= 5 && popularBernie[4].followers < followers){
+        popularBernie.pop();
+        popularBernie.push(newPost);
+        popularBernie.sort(sortByFollowers);
+      }else if (popularBernie.length < 5){
+        popularBernie.push(newPost);
+        popularBernie.sort(sortByFollowers);
       }
+      console.log(popularBernie);
+    }
+
+    else if (hashtags.includes('hillaryclinton')) {
+      var hillary = $scope.pplData.hillary;
+      hillary.numTallied++;
+      hillary.totalFollowers += +followers;
+      $scope.followersData[2] = hillary.totalFollowers / hillary.numTallied;
+
+
+      var popularHillary = $scope.mostPopular.hillary;
+      console.log(tweet.text);
+      var newPost = {message: $scope.tweet, followers: +followers, user: tweet.user.screen_name}
+      if(popularHillary.length === 0){
+        popularHillary.push(newPost)
+      }else if (popularHillary.length >= 5 && popularHillary[4].followers < followers){
+        popularHillary.pop();
+        popularHillary.push(newPost);
+        popularHillary.sort(sortByFollowers);
+      }else if (popularHillary.length < 5){
+        popularHillary.push(newPost);
+        popularHillary.sort(sortByFollowers);
+      }
+      console.log(popularHillary);
     }
   });
 }
